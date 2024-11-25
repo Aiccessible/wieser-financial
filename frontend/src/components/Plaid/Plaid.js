2cde1c7a742d4796afd29f21d74f4ec03570bd44
OwenStadlwieser <47876306+OwenStadlwieser@users.noreply.github.com>
2024-11-24 16:52:30 -0800

    web only

--- frontend/src/components/Plaid/Plaid.js
+++ frontend/src/components/Plaid/Plaid.js
@@ -1,8 +1,9 @@
 import {  post } from 'aws-amplify/api';
-import { ConsoleLogger } from 'aws-amplify/utils';
 import PlaidLink from '../Plaid/PlaidLink';
 import { usePlaidHooks } from '../hooks/usePlaidHooks';
 import { View, TouchableOpacity, Text } from 'react-native';
+import { Button } from '@aws-amplify/ui-react';
+import { CustomTextBox } from '../common/Custom/CustomTextBox';
 
 const apiName = "plaidapi";
 
@@ -15,22 +16,22 @@ export default function Plaid({ getItems }) {
   } = usePlaidHooks({ getItems, getPath: '/v1/tokens/get_investment_token' })
   return (
       <View style={{ alignItems: 'center' }}>
-          <TouchableOpacity
-              className="bg-primary px-4 w-[90%] py-3 rounded-lg items-center justify-center mb-4"
+          <Button
+              className="bg-white px-4 w-[90%] py-3 rounded-lg items-center transition duration-300 hover:bg-green-700 ease-in-out justify-center mb-4"
               variation="primary"
               isLoading={connecting}
               onPress={handleGetToken}
           >
-              <Text>CONNECT BANK ACCOUNT PLAID</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-              className="bg-primary px-4 py-3 w-[90%] rounded-lg items-center justify-center mb-4"
+              <p className="text-black">ADD BANK ACCOUNT </p>
+          </Button>
+          <Button
+              className="bg-white px-4 py-3 w-[90%] rounded-lg items-center transition duration-300 hover:bg-green-700 ease-in-out justify-center mb-4"
               variation="primary"
               isLoading={connecting}
               onPress={handleGetStockToken}
           >
-              <Text>CONNECT INVESTMENT ACCOUNT WITH PLAID</Text>
-          </TouchableOpacity>
+              <p className="text-black whitespace-nowrap">ADD INVESTMENT ACCOUNT </p>
+          </Button>
           {token ? <PlaidLink token={token} onSuccess={handleSuccess} onExit={() => setConnecting(false)} /> : null}
           {stockToken ? (
               <PlaidLink
